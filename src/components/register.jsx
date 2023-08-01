9b762fb590dc0ad38a45a4c08c0a1ca5ebfcc6d4
Samuel <santillans226@gmail.com>
2023-07-31 21:04:02 -0300

    cracion de la branch

---
+++ src/components/register.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+
+import "./register.css"
+
+const register = () => {
+  return (
+    <Container>
+     <h1> register </h1>
+    </Container>
+  );
+};
+
+export default register; 
