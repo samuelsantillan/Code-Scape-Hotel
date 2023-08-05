31e08735ac11bb4870d464488bd47c7559559c86
Victoria Cavanna <viccavanna@gmail.com>
2023-08-05 00:32:35 -0300

    Creación branch footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './footerStyle.css';
+import { Container } from 'react-bootstrap';
+import logoBeige from '../../../assets/logoFinalBeige.svg';
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h1>Footer</h1>
+    </footer>
+  );
+}
+
+export default Footer;
