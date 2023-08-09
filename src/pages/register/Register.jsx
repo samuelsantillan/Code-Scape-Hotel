eb67355441c15a8dfd93517e1b1fb746107fc779
Samuel <santillans226@gmail.com>
2023-08-09 03:04:32 -0300

    nuevo maquetado login/register

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -7,35 +7,60 @@ import '../register/register.css'
 
 const Register = () => {
   return (
-  <div className='contenedor'>
-    <Form>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Dirección de correo electronico</Form.Label>
-      <Form.Control type="email" placeholder="Ingrese su correo " />
-      <Form.Text className="text-muted">
-        No vamos a compartir su correo con nadie mas.
-      </Form.Text>
-    </Form.Group>
+  <div className='container'>
+   <div class="container2">
+  <div class="form-login">
+    <h2>Bienvenido de nuevo</h2>
+    <label>
+      <span>Email</span>
+      <input type="email" />
+    </label>
+    <label>
 
-    <Form.Group className="mb-3" controlId="formBasicPassword">
-      <Form.Label>Contraseña</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-      
-    </Form.Group>
-    
-    <Form.Group className="mb-3" controlId="formBasicPassword">
-      <Form.Label>Confirme su contraseña</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-  
-    </Form.Group>
-    <Button variant="danger" type="submit">
-      Crear cuenta
-    </Button>
-  </Form>
+      <span>Password</span>
+      <input type="password" />
+    </label>
+    <p class="recovery-password">Recuperar contraseña </p>
+    <button type="button" class="submit">Iniciar sesion</button>
+
+  </div>
+  <div class="container3">
+    <div class="img">
+      <div class="img-register">
+        <h2>¿No tenes cuenta?</h2>
+        <p>Registrate</p>
+      </div>
+      <div class="img-login">
+        <h2>¿Ya tenes una cuenta?</h2>
+        <p>Ingresá</p>
+      </div>
+      <div class="img-btn">
+        <span class="register">registrate </span>
+        <span class="login">Ingresá</span>
+      </div>
+    </div>
+    <div class="form-register">
+      <h2>Registrate aqui</h2>
+      <label>
+        <span>Nombre</span>
+        <input type="text" />
+      </label>
+      <label>
+        <span>Email</span>
+        <input type="email" />
+      </label>
+      <label>
+        <span>Contraseña</span>
+        <input type="password" />
+      </label>
+      <button type="button" class="submit">Sign Up</button>
+   
+    </div>
+  </div>
+</div>
+
+
+
   </div>
   )
 }
