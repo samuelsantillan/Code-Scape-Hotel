7fcfcb5d1a2be82b2116394c2ff5ce318b20a63d
azafe <alvarozafe@gmail.com>
2023-08-10 12:10:14 -0300

    Se añade RoomPagePreview

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import RoomList from './components/room/RoomList';
-import ReservationForm from './components/room/ReservationForm';
-import './App.css'
-import RoomGrid from './components/room/RoomGrid';
-import NavBarBorrar from './components/room/navbarBorrar';
-import RoomPreview from './components/room/RoomPreview';
-
-
+import React from "react";
+import RoomList from "./components/room/RoomList";
+import ReservationForm from "./components/room/ReservationForm";
+import "./App.css";
+import RoomGrid from "./components/room/RoomGrid";
+import NavBarBorrar from "./components/room/navbarBorrar";
+import RoomPreview from "./components/room/RoomPreview";
+import RoomPagePreview from "./components/RoomPagePreview";
 
 const App = () => {
   return (
-    <div >
+    <div>
       {/* <h1 className='font-title'>1 Cama doble</h1> */}
       <NavBarBorrar />
-      <RoomGrid />
-      <RoomPreview />
+      {/* <RoomGrid /> */}
+      
+     <RoomPagePreview />
+
       {/* <RoomList rooms={roomsData} /> */}
       {/* <ReservationForm rooms={roomsData} /> */}
     </div>
--- src/components/room/RoomPreview.jsx
+++ src/components/room/RoomPreview.jsx
@@ -1,45 +1,75 @@
 import React from "react";
-import "./RoomPreview.css"
+import "./RoomPreview.css";
+import { Container, Row, Col, Image } from "react-bootstrap";
 
-const RoomPreview = () => {
-  <div className="restaurantSection">
-    <Container>
-      <Row>
-        <Col
-          xs={12}
-          lg={6}
-          className="restaurantImages imgCol col p-0 d-flex justify-content-center"
-        >
-          <div className="restaurantImg">
-            <div className="image-wrapper">
-              <Image
-                src="https://images.trvl-media.com/lodging/2000000/1330000/1322200/1322111/c8ba6d52.jpg?impolicy=resizecrop&rw=1200&ra=fit"
-                alt="Foto Hotel"
-                fluid
-                onMouseOver={() =>
-                  handleRoomHover(
-                    "https://images.trvl-media.com/lodging/2000000/1330000/1322200/1322111/c8ba6d52.jpg?impolicy=resizecrop&rw=1200&ra=fit"
-                  )
-                }
-                onMouseOut={() => handleRoomHover()}
-              />
-            </div>
-          </div>
-        </Col>
-        <Col xs={12} lg={6} className="restaurantTextCol textCol col">
-          <p>RESTAURANTE</p>
-          <h3>Experiencia completa</h3>
-          <p>
-            En CodeScape, deleita tus sentidos con una cocina de primera calidad
-            que celebra los ingredientes locales más frescos. Acompaña tu comida
-            con una selecta copa de vino y vive un deleite culinario en un
-            ambiente inigualable.{" "}
-          </p>
-        </Col>
-      </Row>
-    </Container>
-  </div>;
+const RoomPreview = (props) => {
+  const { IsImageFirst } = props;
+
+  return (
+    <div className="restaurantSection my-5 py-5" >
+      <Container>
+        <Row>
+          {IsImageFirst ? (
+            <>
+              <Col
+                xs={12}
+                lg={6}
+                className="d-flex justify-content-center"
+              >
+                <div className="restaurantImg">
+                  <div className="image-wrapper">
+                    <Image
+                      src={props.image}
+                      alt="Foto Hotel"
+                      fluid
+                      // onMouseOver={() =>
+                      //   handleRoomHover(
+                      //     '"https://images.trvl-media.com/lodging/2000000/1330000/1322200/1322111/c8ba6d52.jpg?impolicy=resizecrop&rw=1200&ra=fit"'
+                      //   )
+                      // }
+                      // onMouseOut={() => handleRoomHover()}
+                    />
+                  </div>
+                </div>
+              </Col>
+              <Col xs={12} lg={6} className="restaurantTextCol textCol col">
+                <h3>{props.name}</h3>
+                <p>{props.description} </p>
+              </Col>
+            </>
+          ) : (
+            <>
+              <Col xs={12} lg={6} className="restaurantTextCol textCol col">
+                <h3>{props.name}</h3>
+                <p>{props.description} </p>
+              </Col>
+              <Col
+                xs={12}
+                lg={6}
+                className="restaurantImages imgCol col p-0 d-flex justify-content-center"
+              >
+                <div className="restaurantImg">
+                  <div className="image-wrapper">
+                    <Image
+                      src={props.image}
+                      alt="Foto Hotel"
+                      fluid
+                      // onMouseOver={() =>
+                      //   handleRoomHover(
+                      //     "https://images.trvl-media.com/lodging/2000000/1330000/1322200/1322111/c8ba6d52.jpg?impolicy=resizecrop&rw=1200&ra=fit"
+                      //   )
+                      // }
+                      // onMouseOut={() => handleRoomHover()}
+                    />
+                  </div>
+                </div>
+              </Col>
+            </>
+          )}
+        </Row>
+      </Container>
+    </div>
+  );
 };
-
 
 export default RoomPreview;
