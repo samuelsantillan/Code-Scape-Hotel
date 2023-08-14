935d089e14105f425d3c453df33887ed05061e4e
Samuel <santillans226@gmail.com>
2023-08-14 18:30:55 -0300

    botones centrados y solucion de animacion con react

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,68 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import '../register/register.css'
-
-document.querySelector('.img__btn').addEventListener('click', function() {
-  document.querySelector('.cont').classList.toggle('s--signup');
-});
+import '../register/register.css';
 
 const Register = () => {
+  const [isSignupVisible, setIsSignupVisible] = useState(false);
+
+  const toggleSignup = () => {
+    setIsSignupVisible(prevState => !prevState);
+  };
+
   return (
-    
-  
-  <div className='main-container'>
-  <div className='cont'>
-  <div className='form sign-in'>
-    <h2>Bienvenido</h2>
-    <label>
-      <span>Email</span>
-      <input type="email" />
-    </label>
-    <label>
-      <span>Contraseña</span>
-      <input type="password" />
-    </label>
- <div className='button-register'>
-    <button type="button" className='submit'>Registrate</button>
-    </div>
-  </div>
-  <div className='sub-cont'>
-    <div className='img'>
-      <div className='img__text m--up'>
-        <h2>¿sos nuevo?</h2>
-        
+    <div className='main-container'>
+      <div className={`cont ${isSignupVisible ? 's--signup' : ''}`}>
+        <div className='form sign-in'>
+          <h2>Bienvenido</h2>
+          <label>
+            <span>Email</span>
+            <input type="email" />
+          </label>
+          <label>
+            <span>Contraseña</span>
+            <input type="password" />
+          </label>
+          <div className='button-register'>
+            <button type="button" className='submit'>
+              Iniciar sesión
+            </button>
+          </div>
+        </div>
+        <div className='sub-cont'>
+          <div className='img'>
+            <div className={`img__text ${isSignupVisible ? 'm--in' : 'm--up'}`}>
+              <h2>{isSignupVisible ? '¿Ya tenés cuenta?' : '¿Sos nuevo?'}</h2>
+              {isSignupVisible && <p>Ingresá aquí</p>}
+            </div>
+            <div className='img__btn' onClick={toggleSignup}>
+              <span className={`m--up ${isSignupVisible ? 'inactive' : ''}`}>Registrate</span>
+              <span className={`m--in ${isSignupVisible ? '' : 'inactive'}`}>Inicia sesión</span>
+            </div>
+          </div>
+          <div className={`form sign-up ${isSignupVisible ? 'active' : ''}`}>
+            <label>
+              <span>Nombre</span>
+              <input type="text" />
+            </label>
+            <label>
+              <span>Email</span>
+              <input type="email" />
+            </label>
+            <label>
+              <span>Contraseña</span>
+              <input type="password" />
+            </label>
+            <div className='button-register'>
+              <button type="button" className='submit'>
+                Registrate
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className='img__text m--in'>
-        <h2>¿Ya tenes cuenta?</h2>
-        <p>Ingresá aqui</p>
-      </div>
-      <div className='img__btn'>
-        <span className='m--up'>Registrate</span>
-        <span className='m--in'>Inicia sesion</span>
-      </div>
     </div>
-    <div className='form sign-up'>
-      
-      <label>
-        <span>Nombre</span>
-        <input type="text" />
-      </label>
-      <label>
-        <span>Email</span>
-        <input type="email" />
-      </label>
-      <label>
-        <span>Contraseña</span>
-        <input type="password" />
-      </label>
-      <button type="button" className='submit'>Registrate</button>
-   
-    </div>
-  </div>
-</div>
-</div>
-  )
+  );
 }
 
-export default Register
+export default Register;
