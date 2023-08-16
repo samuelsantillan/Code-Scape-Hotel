56b0e68d2b5ad2d992d83f6449528553833f7714
Samuel <santillans226@gmail.com>
2023-08-16 16:13:34 -0300

    test formulario

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -6,29 +6,51 @@ import '../register/register.css';
 const Register = () => {
   const [isSignupVisible, setIsSignupVisible] = useState(false);
 
+  const [nombre, setNombre] = useState('');
+  const [correo, setCorreo] = useState('');
+  const [contraseña, setContraseña] = useState('');
+
   const toggleSignup = () => {
     setIsSignupVisible(prevState => !prevState);
+  };
+
+  const handleLoginSubmit = () => {
+    console.log('Inicio de sesión:','Email:', correo, 'Contraseña:', contraseña);
+
+  };
+
+  const handleSignupSubmit = () => {
+    console.log('Registro:', 'Nombre:', nombre, 'Email:', correo, 'Contraseña:', contraseña);
+
   };
 
   return (
     <div className='main-container'>
       <div className={`cont ${isSignupVisible ? 's--signup' : ''}`}>
-        <div className='form sign-in'>
+        <form className='form sign-in'>
           <h2>Bienvenido</h2>
           <label>
             <span>Email</span>
-            <input type="email" />
+            <input
+              type="email"
+              value={correo}
+              onChange={(e) => setCorreo(e.target.value)}
+            />
           </label>
           <label>
             <span>Contraseña</span>
-            <input type="password" />
+            <input
+              type="password"
+              value={contraseña}
+              onChange={(e) => setContraseña(e.target.value)}
+            />
           </label>
           <div className='button-register'>
-            <button type="button" className='submit'>
+            <button type="button" className='submit' onClick={handleLoginSubmit}>
               Iniciar sesión
             </button>
           </div>
-        </div>
+        </form>
         <div className='sub-cont'>
           <div className='img'>
             <div className={`img__text ${isSignupVisible ? 'm--in' : 'm--up'}`}>
@@ -40,25 +62,37 @@ const Register = () => {
               <span className={`m--in ${isSignupVisible ? '' : 'inactive'}`}>Inicia sesión</span>
             </div>
           </div>
-          <div className={`form sign-up ${isSignupVisible ? 'active' : ''}`}>
+          <form className={`form sign-up ${isSignupVisible ? 'active' : ''}`}>
             <label>
               <span>Nombre</span>
-              <input type="text" />
+              <input
+                type="text"
+                value={nombre}
+                onChange={(e) => setNombre(e.target.value)}
+              />
             </label>
             <label>
               <span>Email</span>
-              <input type="email" />
+              <input
+                type="email"
+                value={correo}
+                onChange={(e) => setCorreo(e.target.value)}
+              />
             </label>
             <label>
               <span>Contraseña</span>
-              <input type="password" />
+              <input
+                type="password"
+                value={contraseña}
+                onChange={(e) => setContraseña(e.target.value)}
+              />
             </label>
             <div className='button-register'>
-              <button type="button" className='submit'>
+              <button type="button" className='submit' onClick={handleSignupSubmit}>
                 Registrate
               </button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
