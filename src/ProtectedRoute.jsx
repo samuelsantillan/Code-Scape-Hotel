f467e7ce9ecc957b55e903065be088b571ae94e6
luki1qq <tu_nuevo_email@example.com>
2023-08-22 20:57:52 -0300

    se modifico el loading de Protected Routes y se cambiaron los acceso

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,15 +1,22 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
+import  LoadingPage  from "./pages/loading/LoadingPage";
+const PRIVILEGED_ROLES = {
+  ADMIN: 0,
+  USER: 1,
+};
 
 export const ProtectedRoute = () => {
   const { isAuthenticated, loading, user } = useAuth();
   console.log(user);
-  if (loading) return <h1>Loading...</h1>;
+  if (loading) return <LoadingPage />;
   console.log(loading, isAuthenticated);
   // if(!isAuthenticated  ) return <Navigate to='/home' replace />
-  if (!isAuthenticated && !loading) return <Navigate to='/login' replace />
+  if (!isAuthenticated && !loading) return <Navigate to="/login" replace />;
   console.log(user.role, isAuthenticated);
-  if(isAuthenticated && user.role === 1) return <Navigate to='/admin' replace />
+  // if(isAuthenticated && user.role === PRIVILEGED_ROLES.ADMIN) return <Navigate to='/admin' replace />
+  if (isAuthenticated && user.role === PRIVILEGED_ROLES.USER)
+    return <Navigate to="/" replace />;
   return <Outlet />;
 };
 
