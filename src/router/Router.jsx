05d81e7263e9b346ac8ad93422c158e222d37c47
luki1qq <tu_nuevo_email@example.com>
2023-08-21 22:24:06 -0300

    se modifico las rutas con limitaciones para la autenticacion

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from '../layout/layout'
-import Register from '../pages/register/Register'
-import Login from '../pages/login/Login'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../layout/layout";
+import Register from "../pages/register/Register";
+import Login from "../pages/login/Login";
+import Home from "../pages/Home";
+import AuthProvider from "../context/AuthContext";
+import ProtectedRoute from "../ProtectedRoute";
+import Admin from "../pages/Admin";
 const Router = () => {
-    
   return (
-    
-    <BrowserRouter> 
-    <Layout>
-        <Routes>
-            <Route path='/register' element={ <Register /> }/>
-            <Route path='/login' element={ <Login /> }/>
-        </Routes>
-        </Layout>
-     </BrowserRouter>
-  )
-}
+    <AuthProvider>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/login" element={<Login />} />
 
-export default Router
+            <Route path="/register" element={<Register />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/home" element={<Home />} />
+              <Route path="/admin" element={<Admin />} />
+            </Route>
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+};
+
+export default Router;
