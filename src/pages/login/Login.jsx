351541a1542b323d5e2ba8d84f6830ed451b51c6
luki1qq <tu_nuevo_email@example.com>
2023-08-21 22:20:59 -0300

    se modifico el formulario y se enlazo a la api

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,106 +1,81 @@
-import React, { useState } from 'react';
-
-import '../login/login.css';
-
-const Register = () => {
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useAuth } from "../../context/AuthContext";
+import "../login/login.css";
+import { Link, useNavigate } from "react-router-dom";
+const Login = () => {
   const [isSignupVisible, setIsSignupVisible] = useState(false);
+  const { signin, user, isAuthenticated, errors: signinErrors } = useAuth();
+  const [nombre, setNombre] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [contraseña, setContraseña] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
 
-  const [nombre, setNombre] = useState('');
-  const [correo, setCorreo] = useState('');
-  const [contraseña, setContraseña] = useState('');
-
-
-
-  const handleLoginSubmit = () => {
-    console.log('Inicio de sesión:','Email:', correo, 'Contraseña:', contraseña);
-
+  const onSubmit = async (value) => {
+    console.log(value);
+    signin(value);
   };
-
-
+  
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/home");
+    }
+  },[isAuthenticated]);
 
   return (
-    <div className='main-container' >
-      <div className={`cont ${isSignupVisible ? 's--signup' : ''}`}>
-        <form className='form sign-in' >
+    <div className="main-container">
+      <div className={`cont ${isSignupVisible ? "s--signup" : ""}`}>
+        <form className="form sign-in" onSubmit={handleSubmit(onSubmit)}>
+        { signinErrors.map((error,i) => ( <p key={i} className="text-center" style={{color: "red",}}>{error}</p>))}
           <h2>BIENVENIDO</h2>
           <label>
             <span>Email</span>
-            <input
-              type="email"
-              value={correo}
-              onChange={(e) => setCorreo(e.target.value)}
-            />
+            <input type="email" {...register("email", { required: true })} />
           </label>
           <label>
             <span>Contraseña</span>
             <input
               type="password"
-              value={contraseña}
-              onChange={(e) => setContraseña(e.target.value)}
+              {...register("password", { required: true })}
             />
           </label>
-          <div className='button-register'>
-            <button type="button" className='submit' onClick={handleLoginSubmit} >
-              Iniciar sesión
-            </button>
+          <div className="button-register">
+            <button type="submit" className="submit">Iniciar sesión</button>
           </div>
         </form>
-        <div className='sub-cont'>
-          <div className='img'>
-            <div className={`img__text ${isSignupVisible ? 'm--in' : 'm--up'}`}>
-              <h2>{isSignupVisible ? '¿Ya tenés cuenta?' : '¿Sos nuevo?'}</h2>
+        <div className="sub-cont">
+          <div className="img">
+            <div className={`img__text ${isSignupVisible ? "m--in" : "m--up"}`}>
+              <h2>{isSignupVisible ? "¿Ya tenés cuenta?" : "¿Sos nuevo?"}</h2>
               {isSignupVisible && <p></p>}
             </div>
-            <div className='img__btn' >
-              <span className={`m--up ${isSignupVisible ? 'inactive' : ''}`}>Registrate</span>
-              <span className={`m--in ${isSignupVisible ? '' : 'inactive'}`}>Inicia sesión</span>
+            <div className="img__btn">
+              <span className={`m--up ${isSignupVisible ? "inactive" : ""}`}>
+                Registrate
+              </span>
+              <span className={`m--in ${isSignupVisible ? "" : "inactive"}`}>
+                Inicia sesión
+              </span>
             </div>
           </div>
-          <form className={`form sign-up ${isSignupVisible ? 'active' : ''}`}>
-            <label>
-              <span>Nombre</span>
-              <input
-                type="text"
-                value={nombre}
-                onChange={(e) => setNombre(e.target.value)}
-              />
-            </label>
-            <label>
-              <span>Email</span>
-              <input
-                type="email"
-                value={correo}
-                onChange={(e) => setCorreo(e.target.value)}
-              />
-            </label>
-            <label>
-              <span>Contraseña</span>
-              <input
-                type="password"
-                value={contraseña}
-                onChange={(e) => setContraseña(e.target.value)}
-              />
-            </label>
-            <div className='button-register'>
-              <button type="button" className='submit' >
-                Registrate
-              </button>
-            </div>
-          </form>
         </div>
       </div>
       <div className="img2">
         {" "}
         <h2>¿No tenes cuenta?</h2>{" "}
         <div className="img__btn">
-      
           <span className={`m--up ${isSignupVisible ? "inactive" : ""}`}>
-            Registrate
+          <Link to="/register" className="submit">Registrate</Link>
           </span>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Register;
+export default Login;
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,58 +1,63 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../../context/AuthContext";
 import "../register/register.css";
 import Login from "../login/login.jsx";
+import { registerRequest } from "../../api/auth";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+// TODO  -> nombre es el usuario (Cambiarlo)
+
 const Register = () => {
   const [isSignupVisible, setIsSignupVisible] = useState(false);
 
-  const [nombre, setNombre] = useState("");
-  const [correo, setCorreo] = useState("");
-  const [contraseña, setContraseña] = useState("");
+  const { signup, user, isAuthenticated, errors: RegisterErrors } = useAuth();
 
-  const handleSignupSubmit = () => {
-    console.log("Nombre:", nombre);
-    console.log("Email:", correo);
-    console.log("Contraseña:", contraseña);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+
+  console.log(user);
+
+  useEffect(() => {
+    if (isAuthenticated) navigate("/");
+  }, [isAuthenticated]);
+
+  console.log();
+
+  const onSubmit = async (value) => {
+    console.log(value);
+    await signup(value);
   };
 
   return (
-   
     <div className="main-container">
-      
       <div className={`cont ${isSignupVisible ? "s--signup" : ""}`}>
-        
-        <form className={`form sign-up ${isSignupVisible ? "active" : ""}`}>
+        { RegisterErrors.map((error,i) => ( <p key={i} className="text-center" style={{color: "red",}}>{error}</p>))}
+        <form
+          className={`form sign-up ${isSignupVisible ? "active" : ""}`}
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <h2>CREAR CUENTA</h2>
           <label>
             <span>Nombre</span>
-            <input
-              type="text"
-              value={nombre}
-              onChange={(e) => setNombre(e.target.value)}
-            />
+            <input {...register("username")} type="text" autoFocus />
           </label>
+
+          { errors.username &&   <p className="text-center" style={{color: "red",}}>username is required</p>}
           <label>
             <span>Email</span>
-            <input
-              type="email"
-              value={correo}
-              onChange={(e) => setCorreo(e.target.value)}
-            />
+            <input type="email" {...register("email")} />
+            {errors.email && <p>Email Is required</p>}
           </label>
           <label>
             <span>Contraseña</span>
-            <input
-              type="password"
-              value={contraseña}
-              onChange={(e) => setContraseña(e.target.value)}
-            />
+            <input type="password" {...register("password")} />
           </label>
           <div className="button-register">
-            <button
-              type="button"
-              className="submit "
-              onClick={handleSignupSubmit}
-            >
+            <button type="submit" className="submit ">
               Registrate
             </button>
           </div>
@@ -65,20 +70,17 @@ const Register = () => {
               {isSignupVisible && <p></p>}
             </div>
             <div className="img__btn">
-              
               <span className={`m--up ${isSignupVisible ? "inactive" : ""}`}>
                 Entrar
               </span>
             </div>
           </div>
         </div>
-       
       </div>
       <div className="img2 ">
         {" "}
         <h2>¿Ya tenés cuenta?</h2>
         <div className="img__btn">
-      
           <span className={`m--up ${isSignupVisible ? "inactive" : ""}`}>
             Entrar
           </span>
