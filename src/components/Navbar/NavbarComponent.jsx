c2f23b782dbad24986bb11e40860c758bece6860
luki1qq <tu_nuevo_email@example.com>
2023-08-22 21:06:55 -0300

    Se linkeo con login y con el panel de administracion

--- src/components/Navbar/NavbarComponent.jsx
+++ src/components/Navbar/NavbarComponent.jsx
@@ -1,30 +1,34 @@
-import { useState, useEffect } from 'react';
-import { useMediaQuery } from 'react-responsive';
-import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
+import { useState, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
+import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
 import "./navbarStyle.css";
-import whiteLogo from '../../assets/svg/logoWhite.svg';
-import beigeLogo from '../../assets/svg/logoBeige.svg';
+import whiteLogo from "../../assets/svg/logoWhite.svg";
+import beigeLogo from "../../assets/svg/logoBeige.svg";
+import { useAuth } from "../../context/AuthContext";
+
 const NavbarComponent = () => {
   const isLargeScreen = useMediaQuery({ minWidth: 992 });
   const [expand, setExpand] = useState(isLargeScreen);
   const [scrolled, setScrolled] = useState(false);
-  const [logoColor, setLogoColor] = useState('white');
-
+  const [logoColor, setLogoColor] = useState("white");
+  const { isAuthenticated, user } = useAuth();
   const handleScroll = () => {
     const isScrolled = window.scrollY > 0;
     setScrolled(isScrolled);
-    setLogoColor(isScrolled ? 'beige' : 'white');
+    setLogoColor(isScrolled ? "beige" : "white");
   };
   const handleNavbarToggle = () => {
     if (!isLargeScreen) {
       setExpand(!expand);
     }
   };
+  console.log(user);
+
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
   useEffect(() => {
@@ -33,28 +37,55 @@ const NavbarComponent = () => {
 
   return (
     <>
-      <Navbar expand={expand} onToggle={handleNavbarToggle} className={scrolled ? 'scrolled' : ''} fixed="top">
+      <Navbar
+        expand={expand}
+        onToggle={handleNavbarToggle}
+        className={scrolled ? "scrolled" : ""}
+        fixed="top"
+      >
         <Container className="my-2">
-          <Navbar.Brand href="/"><img src={logoColor === 'white' ? whiteLogo : beigeLogo} alt="Logo" /> </Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img
+              src={logoColor === "white" ? whiteLogo : beigeLogo}
+              alt="Logo"
+            />{" "}
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
 
           <Navbar.Offcanvas
             id={`offcanvasNavbar-expand-${expand}`}
             aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-            placement="end" className="sideNavbar"
+            placement="end"
+            className="sideNavbar"
           >
             <Offcanvas.Header closeButton>
-              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-              </Offcanvas.Title>
+              <Offcanvas.Title
+                id={`offcanvasNavbarLabel-expand-${expand}`}
+              ></Offcanvas.Title>
             </Offcanvas.Header>
-            <Offcanvas.Body className='d-flex navBody'>
+            <Offcanvas.Body className="d-flex navBody">
               <Nav>
                 <Nav.Link href="/habitaciones">Habitaciones</Nav.Link>
                 <Nav.Link href="/galeria">Galería</Nav.Link>
                 <Nav.Link href="/nosotros">Nosotros</Nav.Link>
                 <Nav.Link href="/contacto">Contacto</Nav.Link>
               </Nav>
-              <Nav.Link href="/login" className="my-2">Iniciar Sesión</Nav.Link>
+              {}
+              {!isAuthenticated && (
+                <Nav.Link href="/login" className="my-2">
+                  Iniciar Sesión
+                </Nav.Link>
+              )}
+              {isAuthenticated && (
+                <Nav.Link href="/logout" className="my-2">
+                  Cerrar Sesión
+                </Nav.Link>
+              )}
+              {isAuthenticated && user.role === 0 && (
+                <Nav.Link href="/admin" className="my-2">
+                  Panel Admin
+                </Nav.Link>
+              )}
             </Offcanvas.Body>
           </Navbar.Offcanvas>
         </Container>
