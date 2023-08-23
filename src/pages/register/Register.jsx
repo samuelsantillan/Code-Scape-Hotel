6093a6b568d1dbff8c2851f9d9f04d75c6a1d0a5
Samuel <santillans226@gmail.com>
2023-08-23 03:42:14 -0300

    fix estilos

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -34,64 +34,51 @@ const Register = () => {
 
   return (
     <div className="main-container-register">
-      
-      <div className={`cont-register   ${isSignupVisible ? "s--signup" : ""}`}>
-        { RegisterErrors.map((error,i) => ( <p key={i} className="text-center" style={{color: "red",}}>{error}</p>))}
-        <form
-          className={`form-register sign-up ${isSignupVisible ? "active" : ""}`}
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <h2>CREAR CUENTA</h2>
-          <label>
-            <span>Nombre</span>
-            <input {...register("username")} type="text" autoFocus />
-          </label>
-
-          { errors.username &&   <p className="text-center" style={{color: "red",}}>username is required</p>}
-          <label>
+      <div className={`cont-register ${isSignupVisible ? "s--signup" : ""}`}>
+        <form className="form-register sign-in" onSubmit={handleSubmit(onSubmit)}>
+        { signinErrors.map((error,i) => ( <p key={i} className="text-center" style={{color: "red",}}>{error}</p>))}
+          <h2 className="h2-register">BIENVENIDO</h2>
+          <label className="label-register" >
             <span>Email</span>
-            <input type="email" {...register("email")} />
-            {errors.email && <p>Email Is required</p>}
+            <input  className="input-register" type="email" {...register("email", { required: true })} />
           </label>
-          <label>
+          <label className="label-register">
             <span>Contraseña</span>
-            <input type="password" {...register("password")} />
-          </label>
+            <input className="input-register"
+              type="password"
+              {...register("password", { required: true })}
+            />
+          </label >
           <div className="button-register">
-            <button type="submit-register" className="submit-register ">
-              Registrate
-            </button>
+            <button type="submit-register" className="submit-register button-register2">Iniciar sesión</button>
           </div>
         </form>
-
         <div className="sub-cont-register">
           <div className="img-register">
-            <div className={`img__text "m--in" : "m--up"}`}>
-              <h2> ¿Ya tenés cuenta?</h2>
-              
+            <div className={`img__text ${isSignupVisible ? "m--in" : "m--up"}`}>
+              <h2 className="h2-register">{isSignupVisible ? "¿Ya tenés cuenta?" : "¿Sos nuevo?"}</h2>
               {isSignupVisible && <p></p>}
             </div>
             <div className="img__btn">
-              
-            <a href="/login"> <span className={`m--up ${isSignupVisible ? "inactive" : ""}`}>
-                Entrar
+            <a href="/register"> <span className={`m--up ${isSignupVisible ? "inactive" : ""}`}>
+                registrate
               </span></a>
+              <span className={`m--in ${isSignupVisible ? "" : "inactive"}`}>
+                Inicia sesión
+              </span>
             </div>
           </div>
         </div>
       </div>
-      <div className="img2-register ">
+      <div className="img2-register">
         {" "}
-        <h2>¿Ya tenés cuenta?</h2>
+        <h2 className="h2-register">¿No tenes cuenta?</h2>{" "}
         <div className="img__btn">
-        <a href="/login"> <span className={`m--up ${isSignupVisible ? "inactive" : ""}`}>
-                Entrar
+        <a href="/register"> <span className={`m--up ${isSignupVisible ? "inactive" : ""}`}>
+                Registrate
               </span></a>
-          
         </div>
-        
       </div>
-      
     </div>
   );
 };
