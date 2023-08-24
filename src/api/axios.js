4a4aec2c8305c1cb1b235e98df8a29d107a70440
luki1qq <tu_nuevo_email@example.com>
2023-08-23 23:10:20 -0300

    logout funcionando

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 const instance = axios.create(
     {
 
-        baseURL: 'https://api.hotelcodescape.tech/api/',
+        baseURL: 'http://localhost:3000/api/',
         withCredentials: true
     }
 )
