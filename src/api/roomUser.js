cd826124ca77928e3b83500e4febdd960124737f
luki1qq <tu_nuevo_email@example.com>
2023-08-24 04:51:22 -0300

    se elimino funciones que no se utilizaban y se agrego nuevos endpoint

--- src/api/roomUser.js
+++ src/api/roomUser.js
@@ -1,12 +1,10 @@
 import axios from "./axios";
 
 
-export const roomUserRequest = room => axios.post(`roomCreate`, room);
+export const createRoomUserReservation = room => axios.post(`createRoomUserReservation`, room);
 
 export const getRooms = () => axios.get(`roomGet`);
 
 export const getRoomUser = (id) => axios.get(`getAvailableDate/${id}`);
 
-export const updateRoom = (id, room) => axios.put(`roomUpdate/${id}`, room);
-
-export const deleteRoom = (id) => axios.delete(`roomDelete/${id}`);
+export const getRoomUserReservation = (id) => axios.get(`roomUserReservation/${id}`);
