5673405776615d131d6f2ad16f9a2e151108a414
luki1qq <tu_nuevo_email@example.com>
2023-08-24 04:52:04 -0300

    se agrego el endpoint /api/getUser

--- src/api/user.js
+++ src/api/user.js
@@ -11,3 +11,5 @@ export const updateUser = (id, user) => axios.put(`updateUser/${id}`, user);
 export const deleteUser = (id) => axios.delete(`deleteUser/${id}`);
 
 export const createUser = (user) => axios.post(`createUserAdmin`, user);
+
+export const getUser = (id) => axios.get(`getUser/${id}`);
