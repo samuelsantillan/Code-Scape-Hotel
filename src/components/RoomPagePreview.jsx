baa4408e505633012b27dc329c1a7e44ccd73326
luki1qq <tu_nuevo_email@example.com>
2023-08-24 04:53:07 -0300

    se agrego el useparams para recibir el id en /habitaciones/:id

--- src/components/RoomPagePreview.jsx
+++ src/components/RoomPagePreview.jsx
@@ -1,18 +1,19 @@
 import React, { useEffect } from "react";
 import RoomPreview from "./room/RoomPreview";
 import { useRoom } from "../context/RoomContext";
-
-
+import { useAuth } from "../context/AuthContext";
+import { useParams } from "react-router-dom";
 
 const RoomPagePreview = () => {
 
-  
+  const params = useParams();
   const {rooms, getRoomsRequest} = useRoom();
+  const { getUserRequest } = useAuth();
 
-
-  
+  console.log("Esto recibo de params",params);
   useEffect(() => {
     getRoomsRequest();
+    getUserRequest(params._id);
   }, []);
   
   console.log("Esto recibo de rooms",rooms);
