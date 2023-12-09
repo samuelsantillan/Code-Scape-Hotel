8579821fdf70683c657517ec53d270e56d3bdcd7
azafe <alvarozafe@gmail.com>
2023-12-09 16:49:42 -0300

    Deploy on amplify aws

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,9 @@ import axios from 'axios'
 
 
 const instance = axios.create(
-    {
-         baseURL: 'http://localhost:3000/api/',
+    {   
+        baseURL: 'https://main.d26talr5r1zbwb.amplifyapp.com/api/',
+        //  baseURL: 'http://localhost:3000/api/',
        // baseURL: 'https://app.hotelcodescape.tech/api/',
         withCredentials: true
     }
